a9c9bf19d95d101d471f6658505a1ccf211843ec
gdserafini <gdserafini@outlook.com>
2025-04-13 11:38:42 -0300

    add -> eslint config for ts

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTs,
+    },
+    rules: {
+      ...eslintPluginTs.configs.recommended.rules,
+    },
+  },
+];
